7703dc377789d909dc5841e6001be111351d7fa0
Niki <niittymaki.niki@gmail.com>
2019-04-08 02:07:30 +0300

    Some overall functionality

---
+++ scripts.js
@@ -0,0 +1,15 @@
+function toggleContacts() {
+    if(document.getElementById("contacts").style.display === "block"){
+
+        document.getElementById("contacts").style.display = "none";
+    } else {
+        document.getElementById("contacts").style.display = "block";
+    }
+}
+function toggleServices() {
+    if(document.getElementById("services").style.display === "block"){
+        document.getElementById("services").style.display = "none";
+    } else {
+        document.getElementById("services").style.display = "block";
+    }
+}
